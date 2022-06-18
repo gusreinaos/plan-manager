4d6ac2d004965f377f5816822e8d1c161a1d9924
Oscar Reina Gustafsson <90770844+gusreinaos@users.noreply.github.com>
2022-06-18 17:23:41 +0200

    Update Folder Structure
    Updated each layer required components in the overall program structure.

--- PlanManager.API/Controllers/WeatherForecastController.cs
+++ PlanManager.API/Controllers/WeatherForecastController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace PlanManager.API.Controllers;
 
 [ApiController]
-[Route("[controller]")]
+[Route("weather-forecast")]
 public class WeatherForecastController : ControllerBase
 {
     private static readonly string[] Summaries = new[]
